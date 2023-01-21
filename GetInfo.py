292dd2112ab4eea3481c425c7086cdeffb2150b2
TechnoDexx <itshark@mail.ru>
2023-01-21 18:07:36 +0300

    Initial commit

---
+++ GetInfo.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3.10
+import GetInfo as Gi
+import sys
+import subprocess as sp
+
+icons = []
+geticon = False
+
+
+def main(argv):
+    global geticon, icons
+    sp.call("clear")
+    try:
+        _url = argv[1]
+
+    except IndexError:
+        print("Введите URL")
+        exit(0)
+    else:
+        icons, geticon = Gi.get_icon(argv[1])
+        try:
+            print("\n Url: {}".format(argv[1]))
+            print("\n Title: {}".format(Gi.get_title(argv[1])))  # get_title(argv[1])))
+        except IndexError:
+            exit(0)
+        except UnboundLocalError:
+            exit(0)
+        print("\n Icon: {}".format(icons[0]))
+
+
+if __name__ == '__main__':
+    main(sys.argv)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='GetInfo',
+    version='0.0.1',
+    packages=['GetInfo'],
+    url='',
+    license='',
+    author='Igor Golov',
+    author_email='igor.programmer@gmail.com',
+    description='Lib for getting favicon and title from URL'
+)
