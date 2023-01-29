0ca0a1df6ee5e2b1f95d5b96372ef73c4bf80254
TechnoDexx <itshark@mail.ru>
2023-01-29 16:39:17 +0300

    Bringing the library to the correct form

--- GetInfo.py
+++ GetInfo.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3.10
-import GetInfo as Gi
 import sys
 import subprocess as sp
 # from GetInfo import Icon, Title
@@ -19,15 +18,15 @@ def main(argv):
         print("Введите URL")
         exit(0)
     else:
-        icons, geticon = Icon.get(argv[1])      # get_icon(argv[1])
+        icons, geticon = Icon.get(argv[1])  # get_icon(argv[1])
         try:
             print("\n Url: {}".format(argv[1]))
             print("\n Title: {}".format(Title.get(argv[1])))  # get_title(argv[1])))
+            print("\n Icon: {}".format(icons[0]))
         except IndexError:
             exit(0)
         except UnboundLocalError:
             exit(0)
-        print("\n Icon: {}".format(icons[0]))
 
 
 if __name__ == '__main__':
